a7559fe81756cd137b19a26777d66cb586071ce2
singhpriyanshu5 <singhpriyanshu5@gmail.com>
2016-02-20 03:26:32 +0800

    Made login screen with facebook functionality working

--- app/src/main/java/com/example/priyanshu/health2u/MainActivity.java
+++ app/src/main/java/com/example/priyanshu/health2u/MainActivity.java
@@ -3,20 +3,35 @@ package com.example.priyanshu.health2u;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.parse.Parse;
 import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
 import com.parse.ui.ParseLoginBuilder;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar app_toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        app_toolbar = (Toolbar) findViewById(R.id.toolbar);
+        app_toolbar.setTitle(R.string.app_name);
+        setSupportActionBar(app_toolbar);
 //        try {
 //            PackageInfo info = getPackageManager().getPackageInfo(
 //                    "com.example.priyanshu.health2u",
@@ -43,14 +58,50 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
         //FacebookSdk.sdkInitialize(getApplicationContext());
+       // Log.v("json", loadJSONFromAsset());
+        try {
+            JSONObject obj = new JSONObject(loadJSONFromAsset());
+            JSONArray features_arr = obj.getJSONArray("features");
+//            ArrayList<HashMap<String, String>> formList = new ArrayList<HashMap<String, String>>();
+//            HashMap<String, String> m_li;
+
+            for (int i = 0; i < features_arr.length(); i++) {
+                JSONObject j_obj = features_arr.getJSONObject(i);
+                JSONObject properties_obj = j_obj.getJSONObject("properties");
+                String name=properties_obj.getString("name");
+                Log.v("names", name);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         Parse.initialize(this);
         ParseFacebookUtils.initialize(this);
 //        ParseObject testObject = new ParseObject("TestObject");
 //        testObject.put("foo", "bar");
 //        testObject.saveInBackground();
-        ParseLoginBuilder builder = new ParseLoginBuilder(this);
-        startActivityForResult(builder.build(), 0);
+        if(ParseUser.getCurrentUser()==null) {
+            ParseLoginBuilder builder = new ParseLoginBuilder(this);
+            startActivityForResult(builder.build(), 0);
+        }
 
+
+    }
+
+    public String loadJSONFromAsset(){
+        String json = null;
+        try {
+            InputStream is = this.getAssets().open("pharmacyjson.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
     }
 
     @Override
@@ -74,7 +125,13 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.logout) {
+            ParseUser.logOut();
+            //finish();
+//            Parse.initialize(this);
+    //        ParseFacebookUtils.initialize(this);
+            ParseLoginBuilder builder = new ParseLoginBuilder(this);
+            startActivityForResult(builder.build(), 0);
             return true;
         }
 
